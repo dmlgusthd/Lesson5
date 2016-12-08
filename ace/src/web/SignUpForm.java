6675bf34ba903a2dcbf9aad60524a9f25b3317c5
Administrator <Administrator@smart20115>
2016-12-08 16:09:32 +0900

    s

--- ace/src/web/SignUpForm.java
+++ ace/src/web/SignUpForm.java
@@ -35,7 +35,6 @@ public class SignUpForm extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("SignUpForm doPost 메서드 실행");
-		System.out.println("SignUpForm doPost 메서드 실행");
 		request.setCharacterEncoding("euc-kr");
 		int st_no = Integer.parseInt(request.getParameter("st_no"));
 		System.out.println("st_no : "+ st_no);
